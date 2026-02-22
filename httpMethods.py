a177650a3cd00427ebbde6d37525a96c93d14e80
Syed Muhammad Abdullah <s.m.abdullah02022@gmail.com>
2026-02-22 23:28:31 +0500

    Learning http methods

--- httpMethods.py
+++ httpMethods.py
@@ -9,3 +9,8 @@ class Numbers(BaseModel):
     num1: int
     num2: int
 
+# CREATE
+@app.post("/items/{item_id}")
+def create_item(item_id: int, data: Numbers):
+    fake_db[item_id] = data
+    return {"message": "Created", "data": data}
