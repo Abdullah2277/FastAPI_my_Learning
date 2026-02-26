7e8beb6410f09fdf056cc6384671e0ca2df59f6f
Syed Muhammad Abdullah <s.m.abdullah02022@gmail.com>
2026-02-26 21:32:41 +0500

    Learning Pydantic

--- pydant.py
+++ pydant.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel, Field
 from typing import Optional
 
+app = FastAPI()
+
 class Item(BaseModel):
     name: str
     price: float = Field(gt=0, description="Price must be greater than 0")
     description: Optional[str] = None   # optional field
     in_stock: bool = True               # field with default value
+
+@app.post("/products")
+def create_product(item: Item):
+    return item
 
