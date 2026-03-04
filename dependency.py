aaef65dea2dcaadc3090332527746840f583b68d
Syed Muhammad Abdullah <s.m.abdullah02022@gmail.com>
2026-03-04 16:03:58 +0500

    learning dependency injection...

--- dependency.py
+++ dependency.py
@@ -10,3 +10,10 @@ def get_query_params(skip: int = 0, limit: int = 10):
 def list_products(params: dict = Depends(get_query_params)):
     return {"params": params}
 
+# Real world use case — fake auth dependency
+def verify_token(token: str):
+    if token != "secret123":
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return token
+
+
